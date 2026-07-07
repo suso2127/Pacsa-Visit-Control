4d2f42ca59c47b9fbb104e29fe08a61028c64335
suso2127 <yoseit2127@gmail.com>
2026-07-07 00:12:54 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import dynamic from 'next/dynamic';
-
-const FirebaseClientProvider = dynamic(
-  () => import('@/firebase/client-provider').then((mod) => mod.FirebaseClientProvider),
-  { ssr: false }
-);
+import ClientWrapper from './client-wrapper';
 
 export const metadata: Metadata = {
   title: 'GRUPO PACSA S.A. - PACSA VISIT CONTROL',
@@ -26,10 +21,10 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased selection:bg-primary/20 selection:text-primary">
-        <FirebaseClientProvider>
+        <ClientWrapper>
           {children}
           <Toaster />
-        </FirebaseClientProvider>
+        </ClientWrapper>
       </body>
     </html>
   );
